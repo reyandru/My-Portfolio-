808a08fab340ce7c77b8a623fb020cceca3afe5e
reyandru <bariatareiandrew@gmail.com>
2025-04-07 10:31:37 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const scrollers = document.querySelectorAll(".scroller");
 
-// If a user hasn't opted in for recuded motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
